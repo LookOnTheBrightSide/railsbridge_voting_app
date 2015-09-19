2f8bde3a5cb8987952c1d5d5f8586d17cc7e1eb3
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-09-19 10:01:16 +0200

    changed gemfile for heroku

---
+++ app/models/vote.rb
@@ -0,0 +1,12 @@
+class Vote < ActiveRecord::Base
+	belongs_to :topic
+end
+# ActionController  controller
+# ActiveRecord  model
+# ActionView view
+
+
+# ActiveRecord objects 
+# manipulated 
+# acitve relation generates queries 
+# inherits from ActiveRecord::Base
